f8909258c1de8606ce9ee155d8e524e6176fce47
Twixtify <snakelover92@hotmail.com>
2017-10-29 11:40:38 +0100

    Updated link collection

--- supriseMe.py
+++ supriseMe.py
@@ -12,18 +12,15 @@ def random_video(crawl_length):
     while video <= crawl_length:
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'lxml')
-        video_set = set()  # Empty set, does not save duplicates and is O(1) fast
+        video_set = set()  # Empty set, don't save duplicates, append O(1) speed
         for link in soup.find_all('a', attrs={'href': re.compile("/watch")}):
             href = link.get('href')
-            print(url + href)
-            if len(href) <= 43:  # Remove playlist links
+            if len(href) <= len('/watch?v=dQw4w9WgXcQ'):  # Remove playlist links
                 video_set.add('https://www.youtube.com' + href)
-            if link.string is not None:
-                print(link.string)
-        video_list = list(video_set)  # List append at O(n), linear of size
+        video_list = list(video_set)  # List, append O(n), i.e linear in speed
         url = url.replace(url, video_list[numpy.random.randint(len(video_list))])
-        print("New link: " + url)
+        print("Link ", video, " : " + url)
         video += 1
     webbrowser.open(url)
 
-random_video(10)
+random_video(100)
